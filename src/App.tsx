505b4efab480ca1e203595b84f853698810b2639
= <lepeter125@gmail.com>
2021-01-15 21:14:22 -0800

    set up basic react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import Nav from './components/nav/nav';
 import Home from './components/home/home';
+import Game from './components/game/game';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <header className="App-header">
-        <Home />
+      <Nav/>
+      <header className ="App-header">
+        <Router>
+          <Switch>
+              <Route path ="/" exact component = {Home}/>
+              <Route path ="/game" component= {Game}/>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/game/game.tsx
@@ -0,0 +1,9 @@
+const Game: React.FC = () => {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  };
+
+export default Game;

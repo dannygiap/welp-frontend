f8d8be7f0b85348c51cda19c6ad811f7c331c15d
Danny Giap <dannygiap@gmail.com>
2021-01-25 01:04:40 -0800

    setting up socket connection and working on creating global state

--- src/Socket/Socket.tsx
+++ src/Socket/Socket.tsx
@@ -1,4 +1,17 @@
 import io from 'socket.io-client';
 
-class Socket {}
+class Socket {
+  static socket: SocketIOClient.Socket;
+
+  public static initializeSocket(gameID: string): void {
+    this.socket = io('http://localhost:4000/', {
+      transports: ['websocket'],
+      query: `gameID=${gameID}`,
+    });
+  }
+
+  public static getSocket(): SocketIOClient.Socket {
+    return this.socket as SocketIOClient.Socket;
+  }
+}
 export default Socket;
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,13 +1,31 @@
 import './home.css';
 import Socket from '../../Socket/Socket';
+import { useState } from 'react';
 
 const Home: React.FC = () => {
+  const [gameID, setGameID] = useState('');
+
   return (
     <div id="home-container">
       <h2 id="greeting">Hi, dannygiap</h2>
 
-      <form id="game-form">
-        <input type="text" placeholder="Game ID" className="input" />
+      <form
+        id="game-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (gameID === '') return;
+          Socket.initializeSocket(gameID);
+          Socket.getSocket().emit('join', gameID);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Game ID"
+          className="input"
+          onChange={(e) => {
+            setGameID(e.target.value);
+          }}
+        />
         <input type="submit" value="Join Game" className="button red" />
       </form>
 
